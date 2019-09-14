ded9b056e23f4e29b5fab6f0c9d9d7b2e6a928c6
Paul Coelho <pcoelho@Pauls-Mac-Pro.local>
2019-09-14 14:21:47 -0700

    dimension set test

--- typra.ex
+++ typra.ex
@@ -17,7 +17,7 @@
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --]]
 
-loadfile("type.inc")()
+assert(loadfile("type.inc"))()
 
 Enum1 = {
 	"Value11",
@@ -67,35 +67,108 @@ function structOf(str)
 	if str == "Struct1" then return Struct1 end
 	return {}
 end
+Stimulus = {
+	{"findString(\"hello ok again and again\",\"hello\")"},
+	{"findString(\"hello ok again() and again\",\"again\")"},
+	{"findString(\"hello() ok again and again\",\"hello\")"},
+	{"Enum1"},
+	{"Enum2"},
+	{"allOf(Enum1)"},
+	{"allBefore(Enum1,\"Value12\")"},
+	{"allExcept(Enum1,allBefore)"},
+	{"unionSet(allBefore,allExcept)"},
+	{"differSet(allOf,allBefore)"},
+	{"equalSet(allExcept,differSet)"},
+	{"equalSet(allOf,allExcept)"},
+	{"allBefore(Enum1,\"Value13\")"},
+	{"interSet(differSet,allOf)"},
+	{"structTagSpace(Struct1)","structTagSpace(Struct1)"},
+	{"interDimSet({[\"field6\"]=allBefore},structTagSpace,structTagSpace)","structTagSpace"},
+	{"differDimSet(structTagSpace,interDimSet,structTagSpace)","structTagSpace"},
+}
+Expected = {
+	"1,-5",
+	"10,5;22,-5",
+	"1,5",
+	"Value11,Value12,Value13",
+	"Value21,Value22,Value23",
+	"Value11,Value12,Value13",
+	"Value11,(Value12),(Value13)",
+	"(Value11),Value12,Value13",
+	"Value11,Value12,Value13",
+	"(Value11),Value12,Value13",
+	"true",
+	"false",
+	"Value11,Value12,(Value13)",
+	"(Value11),Value12,Value13",
+	"field6:Value11,Value12,Value13;field7:Value21,Value22,Value23",
+	"field6:Value11,Value12,(Value13);field7:Value21,Value22,Value23",
+	"field6:(Value11),(Value12),(Value13);field7:(Value21),(Value22),(Value23)",
+}
+Monitor = {
+	"showFind",
+	"showFind",
+	"showFind",
+	"showList",
+	"showList",
+	"showSet",
+	"showSet",
+	"showSet",
+	"showSet",
+	"showSet",
+	"showBool",
+	"showBool",
+	"showSet",
+	"showSet",
+	"showDimSet",
+	"showDimSet",
+	"showDimSet",
+}
 file = io.open("type.txt","w")
 io.output(file)
-io.write("Enum1 "..showList(Enum1).."\n")
-io.write("Enum2 "..showList(Enum2).."\n")
-io.write("allOf(Enum1) "..showSet(allOf(Enum1)).."\n")
-io.write("allBefore(Enum1,\"Value12\") "..showSet(allBefore(Enum1,"Value12")).."\n")
-io.write("allExcept(Enum1,allBefore) "..showSet(allExcept(Enum1,allBefore(Enum1,"Value12"))).."\n")
-io.write("unionSet(allBefore,allExcept) "..showSet(unionSet(allBefore(Enum1,"Value12"),allExcept(Enum1,allBefore(Enum1,"Value12")))).."\n")
-io.write("differSet(allOf,allBefore) "..showSet(differSet(allOf(Enum1),allBefore(Enum1,"Value12"))).."\n")
-io.write("equalSet(allExcept,differSet) "..showBool(equalSet(allExcept(Enum1,allBefore(Enum1,"Value12")),differSet(allOf(Enum1),allBefore(Enum1,"Value12")))).."\n")
-io.write("equalSet(allOf,AllExcept) "..showBool(equalSet(allOf(Enum1),allExcept(Enum1,allBefore(Enum1,"Value12")))).."\n")
-io.write("allBefore(Enum1,\"Value13\") "..showSet(allBefore(Enum1,"Value13")).."\n")
-io.write("interSet(differSet,allBefore) "..showSet(interSet(differSet(allOf(Enum1),allBefore(Enum1,"Value12")),allBefore(Enum1,"Value13"))).."\n")
-io.write("structTagSpace(Struct1) "..showDimSet(structTagSpace(Struct1),structTagSpace(Struct1)))
+abbrev = {}
+for k,v in ipairs(Stimulus) do
+	chunk = "return "..Monitor[k].."("
+	for j,u in ipairs(v) do
+		expand = u
+		for key,val in pairs(abbrev) do
+			found = findString(expand,key)
+			delta = 0
+			for index,value in ipairs(found) do
+				pos = value[1]
+				len = value[2]
+				if len < 0 then
+					neg = -len
+					sum = pos+delta
+					if sum > 1 then before = string.sub(expand,1,sum-1) else before = "" end
+					if sum+neg <= string.len(expand) then after = string.sub(expand,sum+neg,-1) else after = "" end
+					expand = before..val..after
+					delta = val:len()-neg
+				end
+			end
+		end
+		if j > 1 then chunk = chunk.."," end
+		chunk = chunk..expand
+	end
+	chunk = chunk..")"
+	-- print(chunk)
+	func = assert(load(chunk))
+	actual = func()
+	io.write(v[1].." "..actual.."\n")
+	found = {string.find(v[1],"^%a+%(")}
+	if found and found[1] and found[2] then
+		pos = found[1]
+		lim = found[2]
+		key = string.sub(v[1],pos,lim-1)
+		abbrev[key] = expand
+	end
+end
 io.close(file)
 file = io.open("type.txt","r")
 io.input(file)
-line = io.read(); if line ~= "Enum1 Value11,Value12,Value13" then print("error: "..line); io.exit() end
-line = io.read(); if line ~= "Enum2 Value21,Value22,Value23" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "allOf(Enum1) Value11,Value12,Value13" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "allBefore(Enum1,\"Value12\") Value11,(Value12),(Value13)" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "allExcept(Enum1,allBefore) (Value11),Value12,Value13" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "unionSet(allBefore,allExcept) Value11,Value12,Value13" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "differSet(allOf,allBefore) (Value11),Value12,Value13" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "equalSet(allExcept,differSet) true" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "equalSet(allOf,AllExcept) false" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "allBefore(Enum1,\"Value13\") Value11,Value12,(Value13)" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "interSet(differSet,allBefore) (Value11),Value12,(Value13)" then print("error: "..line); os.exit() end
-line = io.read(); if line ~= "structTagSpace(Struct1) Value11,Value12,Value13;Value21,Value22,Value23" then print("error: "..line); os.exit() end
+for k,v in ipairs(Expected) do
+	line = io.read(); if line ~= Stimulus[k][1].." "..v then print("error: "..line); os.exit() end
+end
 line = io.read(); if line ~= nil then print("error: "..line); os.exit() end
 io.close(file)
-print("typera.ex")
+print("typra.ex")
