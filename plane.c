9890da54fdab4ad8f0c9014a23a7743ae5ba2e56
individkid <paulrrco@gmail.com>
2019-12-13 07:30:15 -0800

    glfw pselect wait

--- plane.c
+++ plane.c
@@ -28,11 +28,18 @@
 #include <pthread.h>
 #include <sys/errno.h>
 
-jmp_buf errbuf = {0};
+jmp_buf mainbuf = {0};
+jmp_buf threadbuf = {0};
+pthread_mutex_t mutex = {0};
+pthread_cond_t cond = {0};
+int sub = 0;
+int hub = 0;
+int trm = 0;
 
 void huberr(const char *str, int num, int arg)
 {
-	longjmp(errbuf,1);
+	if (arg < 0) longjmp(mainbuf,1);
+	else if (arg > 0) longjmp(threadbuf,1);
 }
 
 void exiterr(const char *str, int num, int arg)
@@ -42,48 +49,62 @@ void exiterr(const char *str, int num, int arg)
 
 void *clientmem(void *arg)
 {
-	char **argv = arg;
-	int hub = 0;
-	int sub = 0;
-	struct Client *client = (struct Client*)malloc(sizeof(struct Client));
-	if ((hub = pipeInit(argv[1],argv[2])) < 0) ERROR(exiterr,-1);
-	bothJump(huberr,hub);
-	while (1) {if (setjmp(errbuf) == 0) {
-	for (sub = waitAny(); sub >= 0; sub = waitAny()) {
-	readClient(client,sub);
-	switch (client->mem) {
-	case (Buffer): break;
-	case (Shader): break;
-	default: ERROR(exiterr,-1);}
-	client = (struct Client*)malloc(sizeof(struct Client));}}}
+	int tmp = 0;
+	int goon = 1;
+	while (goon) {if (setjmp(threadbuf) == 0) {
+	for (tmp = waitAny(); tmp >= 0 && goon; tmp = waitAny()) {
+	if (pthread_mutex_lock(&mutex) != 0) ERROR(exiterr,-1);
+	if (tmp == trm) goon = 0; else {
+	sub = tmp; glfwPostEmptyEvent();
+	if (pthread_cond_wait(&cond,&mutex) != 0) ERROR(exiterr,-1);
+	if (pthread_mutex_unlock(&mutex) != 0) ERROR(exiterr,-1);}}}}
 	return 0;
 }
 
 int main(int argc, char **argv)
 {
+	if (pthread_mutex_init(&mutex,0) != 0) ERROR(exiterr,-1);
+	if (pthread_cond_init(&cond,0) != 0) ERROR(exiterr,-1);
 	pthread_t thread = {0};
 	if (argc == 4) {
-	if (pthread_create(&thread,0,clientmem,argv) != 0) ERROR(exiterr,-1);}
+	if ((hub = pipeInit(argv[1],argv[2])) < 0) ERROR(exiterr,-1);
+	if ((trm = openPipe()) < 0) ERROR(exiterr,-1);
+	bothJump(huberr,hub); bothJump(huberr,trm);
+	if (pthread_create(&thread,0,clientmem,0) != 0) ERROR(exiterr,-1);}
 
 	glfwInit();
-
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", 0, 0);
+	struct Client *client = (struct Client*)malloc(sizeof(struct Client));
+	sub = -1;
 
 	uint32_t extensionCount = 0;
 	vkEnumerateInstanceExtensionProperties(0, &extensionCount, 0);
-
-	printf("%d extensions supported",extensionCount);
+	printf("%d extensions supported\n",extensionCount); fflush(stdout);
 
 	while(!glfwWindowShouldClose(window)) {
-	glfwPollEvents();}
+	glfwPollEvents();
+	// send Metric to steer scripts, update other users,
+	//  change modes, sculpt topology, report state
+	if (pthread_mutex_lock(&mutex) != 0) ERROR(exiterr,-1);
+	if (sub >= 0) {
+	readClient(client,sub);
+	switch (client->mem) {
+	case (Buffer): break;
+	case (Shader): break;
+	default: ERROR(exiterr,-1);}
+	client = (struct Client*)malloc(sizeof(struct Client));
+	sub = -1;}
+	if (pthread_cond_signal(&cond) != 0) ERROR(exiterr,-1);
+	if (pthread_mutex_unlock(&mutex) != 0) ERROR(exiterr,-1);}
 
 	glfwDestroyWindow(window);
-
 	glfwTerminate();
 
 	if (argc == 4) {
+	writeInt(1,trm);
 	if (pthread_join(thread,0) != 0) ERROR(exiterr,-1);}
-
+	if (pthread_mutex_destroy(&mutex) != 0) ERROR(exiterr,-1);
+	if (pthread_cond_destroy(&cond) != 0) ERROR(exiterr,-1);
 	return 0;
 }
