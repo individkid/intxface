79b0b8c7945ebaca65a0cbd3fb00ca24da7b8945
individkid <paulrrco@gmail.com>
2019-12-14 08:37:57 -0800

    cpu gpu memory

--- plane.c
+++ plane.c
@@ -26,6 +26,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdint.h>
 #include <sys/errno.h>
 
 jmp_buf jmpbuf = {0};
@@ -36,44 +37,45 @@ int hub = 0;
 int zub = 0;
 
 struct Vertex {
-	int vtxid;
-	int tag[3];
-	float plane[3][3];
-	int versor[3];
-	float coord[3][2];
-	float color[3][3];
-	int texid[3];
-	int facid[3];
-	int matid;
+	int32_t tag[3]; // layout (location=0) in ivec3 tag;
+	float plane[3][3]; // layout (location=1) in vec3 plane[3];
+	int32_t versor[3]; // layout (location=4) in ivec3 versor;
+	float coord[3][2]; // layout (location=5) in vec2 coord[3];
+	float color[4][3]; // layout (location=8) in vec4 color[3];
+	int32_t texid[3]; // layout (location=11) in ivec3 texid;
+	int32_t facid[3]; // layout (location=12) in ivec3 facid;
+	int32_t matid; // layout (location=13) in int matid;
 };
 struct Facet {
-	int facid;
-	int vtxid[3];
+	int32_t vtxid[3]; // just in time filtered per tag
 };
 struct Affine {
-	float view[4][4];
-	float tope[NUMFILE][4][4];
-	float face[4][4];
+	float view[4][4]; // all polytopes at once
+	float tope[NUMFILE][4][4]; // individual polytopes
+	float face[4][4]; // individual plane
 };
 
-int tope = 0;
-int topes = 0;
-struct Affine affine = {0};
-int vertices = 0;
-struct Vertex *vertex = 0;
-int facsts = 0;
-struct Facet *facets = 0;
-enum Matrix matrix = Picture;
-enum Click click = Refine;
-enum Move move = Rotate;
-enum Roll roll = Cylinder;
-enum Type type = Command;
-enum Mode mode = Initial;
+int tag = 0; // which triangles are being rendered
+int tope = 0; // which tope is being transformed
+int plane = 0; // which plane is being transformed
+int topes = 0; // how many files/polytopes there are
+float basis[3][3][3]; // client copy of foot points
+struct Affine affine = {0}; // client copy of transformation
+int vertices = 0; // how many points
+struct Vertex *vertex = 0; // client copy of shader input
+int facets = 0; // how many triangles
+struct Facet *facet = 0; // client copy of point triples
+enum Matrix matrix = Picture; // transformation collection
+enum Click click = Transform; // what mouse click does
+enum Move move = Rotate; // what mouse move does
+enum Roll roll = Cylinder; // what mouse roller does
+enum Type type = Command; // what keyboard does
+enum Mode mode = Initial; // changed by left/right click
 
-struct Affine saved = {0};
-float fixed[3] = {0};
-float moved[2] = {0};
-float rolled = {0};
+struct Affine saved = {0}; // from when sent to file
+float fixed[3] = {0}; // from when pierce point clicked
+float moved[2] = {0}; // from when mouse moved
+float rolled = {0}; // from when roller adjusted
 
 void huberr(const char *str, int num, int arg)
 {
@@ -83,6 +85,15 @@ void huberr(const char *str, int num, int arg)
 void exiterr(const char *str, int num, int arg)
 {
 	exit(arg);
+}
+
+void process(struct Client *client)
+{
+	switch (client->mem) {
+	case (Uniform): break;
+	case (Buffer): break;
+	case (Usage): break;
+	default: ERROR(exiterr,-1);}
 }
 
 void *thread(void *arg)
@@ -130,10 +141,7 @@ int main(int argc, char **argv)
 	if (sub >= 0) {readClient(&client,sub); sub = -1; vld = 1;}
 	if (pthread_cond_signal(&cond) != 0) ERROR(exiterr,-1);
 	if (pthread_mutex_unlock(&mutex) != 0) ERROR(exiterr,-1);}
-	if (vld) {vld = 0; switch (client.mem) {
-	case (Buffer): break;
-	case (Shader): break;
-	default: ERROR(exiterr,-1);}}}}}
+	if (vld) {vld = 0; process(&client);}}}}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
