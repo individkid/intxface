5f0277c0829dad4f8996cf6357399405713fd5cc
individkid <paulrrco@gmail.com>
2020-08-27 19:00:16 -0700

    alloc free rand

--- depend.mk
+++ depend.mk
@@ -4,20 +4,20 @@ facerC.o: face.h
 facerHs: face.hs faceC.o
 facerLua: face.so
 fileC: faceC.o typeC.o
-fileC.o: face.h type.h typer.h
+fileC.o: face.h type.h
 filerLua: face.so type.lua
 lineC: faceC.o typeC.o
-lineC.o: face.h type.h typer.h
+lineC.o: face.h type.h
 meticC.o: metic.h
 planeG.o: plane.h
 planeSw: faceC.o meticC.o plane.so shareC.o typeC.o
-planeSw.o: face.h help.sw metic.h share.h share.sw type.h typer.h
+planeSw.o: face.h help.sw metic.h share.h share.sw type.h
 planraSw: faceC.o meticC.o plane.so shareC.o typeC.o
-planraSw.o: face.h help.sw metic.h share.h share.sw type.h typer.h
-shareC.o: face.h metic.h share.h type.h typer.h
+planraSw.o: face.h help.sw metic.h share.h share.sw type.h
+shareC.o: face.h metic.h share.h type.h
 spaceHs: face.hs faceC.o naive.hs type.hs typeC.o
 tradeC: faceC.o typeC.o
-tradeC.o: face.h type.h typer.h
+tradeC.o: face.h type.h
 type.c: type.src
 type.h: type.src
 type.hs: type.src
@@ -28,8 +28,8 @@ typer.h: type.src typra.src
 typer.hs: type.src typra.src
 typer.lua: type.src typra.src
 typer.sw: type.src typra.src
-typerC: faceC.o
-typerC.o: face.h typer.h
+typerC: faceC.o typeC.o
+typerC.o: face.h type.h typer.h
 typerHs: face.hs faceC.o
 typerLua: face.so
 typerSw: faceC.o
