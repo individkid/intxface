07030df75330d9a686089b04f009ab0c5e7a34a2
Paul Coelho <pcoelho@Pauls-Mac-Pro.local>
2020-09-11 14:23:30 -0700

    place region space

--- spacra.hs
+++ spacra.hs
@@ -23,7 +23,7 @@ import Test.QuickCheck.Test
 import System.Exit
 
 instance Arbitrary Side where
-	arbitrary = frequency [(4, elements [Side 0]), (1, elements [Side 1])]
+ arbitrary = frequency [(4, elements [Side 0]), (1, elements [Side 1])]
 
 prop_boolToSide :: Bool -> Bool
 prop_boolToSide a = sideToBool (boolToSide a) == a
@@ -41,49 +41,61 @@ prop_subsetsI :: [Int] -> [Int] -> [Int]
 prop_subsetsI [] a = a
 prop_subsetsI _ [] = []
 prop_subsetsI (a:b) c = let
-	d = mod a (length c)
-	e = take d c
-	f = drop (d + 1) c
-	g = e Prelude.++ f
-	in prop_subsetsI b g
+ d = mod a (length c)
+ e = take d c
+ f = drop (d + 1) c
+ g = e Prelude.++ f
+ in prop_subsetsI b g
 prop_subsets :: Property
 prop_subsets = forAll (prop_subsetsH 10) $
-	\a -> forAll (prop_subsetsH a) $
-	\b -> forAll (prop_subsetsG a) $
-	\c -> forAll (prop_subsetsG b) $
-	\d -> prop_subsetsF (prop_subsetsI d c) c
+ \a -> forAll (prop_subsetsH a) $
+ \b -> forAll (prop_subsetsG a) $
+ \c -> forAll (prop_subsetsG b) $
+ \d -> prop_subsetsF (prop_subsetsI d c) c
 
 prop_holesF :: [Int] -> [Int] -> Bool
 prop_holesF (a:b:c) (d:e:f)
-	| a < d = (a < b) && (prop_holesF (b:c) (d:e:f))
-	| a > d = (d < e) && (prop_holesF (a:b:c) (e:f))
-	| otherwise = False
-prop_holesF [a] (b:c:d) = (a /= b) && (b < c) && (prop_holesF [] (c:d))
-prop_holesF (a:b:c) [d] = (a /= d) && (a < b) && (prop_holesF (b:c) [])
+ | a < d = (a < b) && (prop_holesF (b:c) (d:e:f))
+ | a > d = (d < e) && (prop_holesF (a:b:c) (e:f))
+ | otherwise = False
+prop_holesF [a] (b:c) = (a /= b) && (prop_holesF [] (b:c))
+prop_holesF (a:b) [c] = (a /= c) && (prop_holesF (a:b) [])
 prop_holesF [] (a:b:c) = (a < b) && (prop_holesF [] (b:c))
 prop_holesF (a:b:c) [] = (a < b) && (prop_holesF (b:c) [])
-prop_holesF [a] [b] = a /= b
-prop_holesF [] _ = True
-prop_holesF _ [] = True
+prop_holesF _ _ = True
 prop_holes :: Property
 prop_holes = forAll (prop_subsetsH 10) $
-	\a -> forAll (prop_subsetsH 10) $
-	\b -> forAll (prop_subsetsG a) $
-	\c -> let
-	d = nub' (map abs c)
-	e = holes b d
-	f = d Naive.++ e
-	in (prop_holesF d e) &&
-	((length e) == b) &&
-	((length f) == (b + (length d))) &&
-	(((indices b) Naive.\\ f) == [])
+ \a -> forAll (prop_subsetsH 10) $
+ \b -> forAll (prop_subsetsG a) $
+ \c -> let
+ d = nub' (map abs c)
+ e = holes b d
+ f = d Naive.++ e
+ in (prop_holesF d e) &&
+ ((length e) == b) &&
+ ((length f) == (b + (length d))) &&
+ (((indices b) Naive.\\ f) == [])
+
+prop_tetrahedron :: Property
+prop_tetrahedron = forAll (vector 4) $
+ \a -> let
+ bounds = map Boundary a
+ power = powerSpace bounds
+ regions = regionsOfPlace power
+ in forAll (Test.QuickCheck.choose (0,((length regions) - 1))) $
+ \b -> let
+ region = regions !! b
+ degen = degenSpace region power
+ space = placeToSpace degen
+ in isLinear 3 space
 
 mainF :: Result -> IO ()
 mainF a
-	| isSuccess a = return ()
-	| otherwise = exitFailure
+ | isSuccess a = return ()
+ | otherwise = exitFailure
 main = do
-	quickCheckResult prop_boolToSide >>= mainF
-	quickCheckResult prop_sideToBool >>= mainF
-	quickCheckResult prop_subsets >>= mainF
-	quickCheckResult prop_holes >>= mainF
+ quickCheckResult prop_boolToSide >>= mainF
+ quickCheckResult prop_sideToBool >>= mainF
+ quickCheckResult prop_subsets >>= mainF
+ quickCheckResult prop_holes >>= mainF
+ quickCheckResult prop_tetrahedron >>= mainF
