30824c1031cb3f0301bbb8f8734d365bdd7a83b1
Paul Coelho <paulrrco@gmail.com>
2022-04-24 22:21:58 -0700

    phase flow stream

--- argx.c
+++ argx.c
@@ -11,17 +11,26 @@ int typ = 0; // type of data to flow
 int stp = 0; // index into program steps
 int mod[NUMMODE] = {0}; // arguments passed to factory
 int mlm = 0; // changing way to handle undashed arguments
-enum ArgxCode und; // for consuming next undashed argument
+int idx = 0; // for constant or immediate expression next
+enum ArgxCode opc; // for consuming next undashed argument
 fftype fac = 0; // factory for undashed arguments
-const char *flg[NUMMODE] = {0}; // char position is mod value
+const char *ach[NUMMODE] = {0}; // char position is mod value
 int adx[NUMMODE] = {0}; // which mod to use
 int alm = 0; // mututally exclusive way to change handling
-const char *nxt[NUMMODE] = {0}; // char position is opcode
-int blm = 0; // next undashed is value for opcode
-const char *dbl[NUMMODE] = {0}; // name of custom stream
-sftype cst[NUMMODE] = {0}; // function for custom stream
-int clm = 0; // double dash like custom undashed
+const char *bch[NUMMODE] = {0}; // char position is mod offset
+int bdx[NUMMODE] = {0}; // base mod to use
+int blm = 0; // constant or lua result for factory argument
+const char *cch[NUMMODE] = {0}; // char position is opcode
+int clm = 0; // next undashed is value for opcode
+const char *dst[NUMMODE] = {0}; // name of custom stream
+sftype dfn[NUMMODE] = {0}; // function for custom stream
+int dlm = 0; // double dash like custom undashed
 
+int getValue(const char *str)
+{
+	// TODO return atoi identifier or lua evaluation
+	return 0;
+}
 struct ArgxCnst getConst(const char *str)
 {
 	struct ArgxCnst cnst = {0};
@@ -38,41 +47,57 @@ fftype setFactory(fftype fnc)
 int addFlags(int idx, const char *chr)
 {
 	adx[alm] = idx;
-	flg[alm] = chr;
+	ach[alm] = chr;
 	return alm++;
+}
+int addConst(int idx, const char *chr)
+{
+	bdx[blm] = idx;
+	bch[blm] = chr;
+	return blm++;
 }
 int addMode(const char *chr)
 {
-	nxt[blm] = chr;
-	return blm++;
+	cch[clm] = chr;
+	return clm++;
 }
 int addCustom(const char *pat, sftype fnc)
 {
-	dbl[clm] = pat;
-	cst[clm] = fnc;
-	return clm++;
+	dst[dlm] = pat;
+	dfn[dlm] = fnc;
+	return dlm++;
 }
 int useArgument(const char *arg)
 {
-	for (int i = 0; i < clm; i++) {
-		if (arg[0] == '-' && arg[1] == '-' && strcmp(arg+2,dbl[i]) == 0) {
+	for (int i = 0; i < dlm; i++) {
+		if (arg[0] == '-' && arg[1] == '-' && strcmp(arg+2,dst[i]) == 0) {
 			nst[nlm].opc = FlowArgx;
-			nst[nlm].fnc = cst[i];
+			nst[nlm].fnc = dfn[i];
 			return nlm++;}}
-	for (int i = 1; i < blm && i < ArgxArgx; i++) {
-		if (arg[0] == '-' && strcmp(arg+1,nxt[i]) == 0) {
-			und = i;
+	for (int i = 1; i < clm && i < ArgxArgx; i++) {
+		if (arg[0] == '-' && strcmp(arg+1,cch[i]) == 0) {
+			opc = i;
 			return nlm;}}
+	for (int i = 0; i < blm; i++) {
+		for (int j = 0; j < strlen(bch[i]); j++) {
+			if (arg[0] == '-' && arg[1] == bch[i][j] && arg[2] == 0) {
+				idx = bdx[i]+j+1;
+				return nlm;}}}
 	for (int i = 0; i < alm; i++) {
-		for (int j = 0; j < strlen(flg[i]); j++) {
-			if (arg[0] == '-' && arg[1] == flg[i][j] && arg[2] == 0) {
+		for (int j = 0; j < strlen(ach[i]); j++) {
+			if (arg[0] == '-' && arg[1] == ach[i][j] && arg[2] == 0) {
 				mod[adx[i]] = j;
 				return nlm;}}}
-	if (und) {
+	if (idx) {
+		mod[idx-1] = getValue(arg);
+		idx = 0;
+		return nlm;}
+	else if (opc) {
 		struct ArgxCnst cnst = getConst(arg);
-		nst[nlm].opc = und;
+		nst[nlm].opc = opc;
 		nst[nlm].str = cnst.str;
 		nst[nlm].idx = cnst.idx;
+		opc = 0;
 		return nlm++;}
 	else if (fac) {
 		struct ArgxFlow flow = fac(arg,mod,mlm);
