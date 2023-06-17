7970ec9be5f9471adb8e386e886bd2188f76d6f9
Paul Coelho <paulrrco@gmail.com>
2023-06-17 10:48:28 -0700

    hint request copy

--- planer.lua
+++ planer.lua
@@ -1,5 +1,5 @@
 dofile("type.lua")
-center = {["cmd"]="SetCmd",["mem"]="Configurez",["siz"]=0,["idx"]=0,["slf"]=0}
+center = {["req"]="SetReq",["mem"]="Configurez",["siz"]=1,["idx"]=0,["slf"]=0,["cfg"]={"RegisterOpen"},["val"]={0}}
 ident = wrapType("Planez","planerLua","metalSw")
 os.execute("sleep 1")
 writeCenter(center,ident)
