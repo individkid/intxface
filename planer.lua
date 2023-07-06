029a017a05057d5090309c39a31bd94922cedad3
Paul Coelho <paulrrco@gmail.com>
2023-07-06 09:59:12 -0700

    assign global pointer

--- planer.lua
+++ planer.lua
@@ -3,8 +3,8 @@ ident = wrapType("Planez","planerLua","metalSw")
 os.execute("sleep 1")
 center = {["mem"]="Configurez",["siz"]=1,["idx"]=0,["slf"]=0,["cfg"]={"PierceSize"},["val"]={1}}
 writeCenter(center,ident)
--- center = {["mem"]="Piercez",["siz"]=1,["idx"]=0,["slf"]=1,["pie"]={{["fix"]={0.0,0.1,0.2,0.3},["nml"]={1.0,1.1,1.2,1.3},["vld"]=1,["idx"]=0,["pad"]={0,0}}}}
--- writeCenter(center,ident)
+center = {["mem"]="Piercez",["siz"]=1,["idx"]=0,["slf"]=1,["pie"]={{["fix"]={0.0,0.1,0.2,0.3},["nml"]={1.0,1.1,1.2,1.3},["vld"]=1,["idx"]=0,["pad"]={0,0}}}}
+writeCenter(center,ident)
 -- center = {["mem"]="Piercez",["siz"]=0,["idx"]=0,["slf"]=2}
 -- writeCenter(center,ident)
 -- center = readCenter(ident)
