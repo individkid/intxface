1b24c8039af9a8e87761eb3885b0a908c30f0023
Paul Coelho <paulrrco@gmail.com>
2023-10-13 21:36:21 -0700

    state change pipe

--- space.hs
+++ space.hs
@@ -32,75 +32,74 @@ mainF [a] = do
  return ()
 
 mainG :: Int -> State -> IO ()
-mainG idx state = do
- change <- readChange idx
+mainG ifd state = do
+ change <- readChange ifd
  str <- newIORef ""
  showChange change str
  val <- readIORef str
  putStrLn val
- mainGF idx state change
-mainGF :: Int -> State -> Change -> IO ()
-mainGF _ _ (Change (ChangeA1 Type.Emergs _ _ _) _) = return ()
-mainGF idx state change = mainG idx (mainH (mainI state change) change)
+ mainH ifd state change
 
-mainH :: State -> Change -> State
-mainH state (Change (ChangeA1 Type.Numerics Type.Towrite idx 0) (ChangeA5B7 [])) = state
-mainH (State a b c d e f g) change@(Change (ChangeA1 Type.Numerics Type.Towrite idx siz) (ChangeA5B7 (s:t))) =
- mainH (State (Map.insert idx s a) b c d e f g) (mainHF change)
-mainH state (Change (ChangeA1 Type.Symbolics Type.Towrite idx 0) (ChangeA5B5 [])) = state
-mainH (State a b c d e f g) change@(Change (ChangeA1 Type.Symbolics Type.Towrite idx siz) (ChangeA5B5 (s:t))) =
- mainH (State a (Map.insert idx s b) c d e f g) (mainHF change)
-mainH state (Change (ChangeA1 Type.Triangles Type.Towrite idx 0) (ChangeA5B6 [])) = state
-mainH (State a b c d e f g) change@(Change (ChangeA1 Type.Triangles Type.Towrite idx siz) (ChangeA5B6 (s:t))) =
- mainH (State a b (Map.insert idx s c) d e f g) (mainHF change)
-mainH state (Change (ChangeA1 Type.Tangents Type.Towrite idx 0) (ChangeA5B5 [])) = state
-mainH (State a b c d e f g) change@(Change (ChangeA1 Type.Tangents Type.Towrite idx siz) (ChangeA5B5 (s:t))) =
- mainH (State a b c (Map.insert idx s d) e f g) (mainHF change)
-mainH state (Change (ChangeA1 Type.Vertexes Type.Towrite idx 0) (ChangeA5B7 [])) = state
-mainH (State a b c d e f g) change@(Change (ChangeA1 Type.Vertexes Type.Towrite idx siz) (ChangeA5B7 (s:t))) =
- mainH (State a b c d (Map.insert idx s e) f g) (mainHF change)
-mainH state (Change (ChangeA1 Type.Vertices Type.Towrite idx 0) (ChangeA5B6 [])) = state
-mainH (State a b c d e f g) change@(Change (ChangeA1 Type.Vertices Type.Towrite idx siz) (ChangeA5B6 (s:t))) =
- mainH (State a b c d e (Map.insert idx s f) g) (mainHF change)
-mainH state (Change (ChangeA1 Type.Polytopes Type.Towrite idx 0) (ChangeA5B5 [])) = state
-mainH (State a b c d e f g) change@(Change (ChangeA1 Type.Polytopes Type.Towrite idx siz) (ChangeA5B5 (s:t))) =
- mainH (State a b c d e f (Map.insert idx s g)) (mainHF change)
-mainH state change = state
-mainHF :: Change -> Change
-mainHF (Change (ChangeA1 cfg tdo idx siz) (ChangeA5B7 (s:t))) =
- (Change (ChangeA1 cfg tdo (idx + 1) (siz - 1)) (ChangeA5B7 t))
+mainH :: Int -> State -> Change -> IO ()
+mainH _ _ (Change (ChangeA1 Type.Emergs _ _ _) _) = return ()
+mainH ifd state (Change (ChangeA1 _ _ _ 0) _) = mainG ifd state
+mainH ifd state (Change (ChangeA1 tag@Type.Numerics act@Type.Towrite idx siz) (ChangeA5B7 (val:lst))) = let
+ nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B7 lst)
+ clr = mainI idx state
+ (State a b c d e f g) = clr
+ set = State (Map.insert idx val a) b c d e f g
+ in mainH ifd set nxt
+mainH ifd state (Change (ChangeA1 tag@Type.Symbolics act@Type.Towrite idx siz) (ChangeA5B5 (val:lst))) = let
+ nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B5 lst)
+ clr = mainI idx state
+ (State a b c d e f g) = clr
+ set = State a (Map.insert idx val b) c d e f g
+ in mainH ifd set nxt
+mainH ifd state (Change (ChangeA1 tag@Type.Triangles act@Type.Towrite idx siz) (ChangeA5B6 (val:lst))) = let
+ nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B6 lst)
+ clr = mainI idx state
+ (State a b c d e f g) = clr
+ set = State a b (Map.insert idx val c) d e f g
+ in mainH ifd set nxt
+mainH ifd state (Change (ChangeA1 tag@Type.Tangents act@Type.Towrite idx siz) (ChangeA5B5 (val:lst))) = let
+ nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B5 lst)
+ clr = mainI idx state
+ (State a b c d e f g) = clr
+ set = State a b c (Map.insert idx val d) e f g
+ in mainH ifd set nxt
+mainH ifd state (Change (ChangeA1 tag@Type.Vertexes act@Type.Towrite idx siz) (ChangeA5B7 (val:lst))) = let
+ nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B7 lst)
+ clr = mainJ idx state
+ (State a b c d e f g) = clr
+ set = State a b c d (Map.insert idx val e) f g
+ in mainH ifd set nxt
+mainH ifd state (Change (ChangeA1 tag@Type.Vertices act@Type.Towrite idx siz) (ChangeA5B6 (val:lst))) = let
+ nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B6 lst)
+ clr = mainJ idx state
+ (State a b c d e f g) = clr
+ set = State a b c d e (Map.insert idx val f) g
+ in mainH ifd set nxt
+mainH ifd state (Change (ChangeA1 tag@Type.Polytopes act@Type.Towrite idx siz) (ChangeA5B5 (val:lst))) = let
+ nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B5 lst)
+ clr = mainK idx state
+ (State a b c d e f g) = clr
+ set = State a b c d e f (Map.insert idx val g)
+ in mainH ifd set nxt
 
-mainI :: State -> Change -> State
-mainI state (Change (ChangeA1 Type.Numerics Type.Towrite idx 0) (ChangeA5B7 [])) = state
-mainI state change@(Change (ChangeA1 Type.Numerics Type.Towrite idx siz) (ChangeA5B7 (s:t))) =
- mainI (mainIF idx state) (mainHF change)
-mainI state (Change (ChangeA1 Type.Symbolics Type.Towrite idx 0) (ChangeA5B7 [])) = state
-mainI state change@(Change (ChangeA1 Type.Symbolics Type.Towrite idx siz) (ChangeA5B7 (s:t))) =
- mainI (mainIF idx state) (mainHF change)
-mainI state (Change (ChangeA1 Type.Triangles Type.Towrite idx 0) (ChangeA5B7 [])) = state
-mainI state change@(Change (ChangeA1 Type.Triangles Type.Towrite idx siz) (ChangeA5B7 (s:t))) =
- mainI (mainIF idx state) (mainHF change)
-mainI state (Change (ChangeA1 Type.Tangents Type.Towrite idx 0) (ChangeA5B7 [])) = state
-mainI state change@(Change (ChangeA1 Type.Tangents Type.Towrite idx siz) (ChangeA5B7 (s:t))) =
- mainI (mainIF idx state) (mainHF change)
-mainI state (Change (ChangeA1 Type.Vertexes Type.Towrite idx 0) (ChangeA5B7 [])) = state
-mainI state change@(Change (ChangeA1 Type.Vertexes Type.Towrite idx siz) (ChangeA5B7 (s:t))) =
- mainI (mainIG idx state) (mainHF change)
-mainI state (Change (ChangeA1 Type.Polytopes Type.Towrite idx 0) (ChangeA5B7 [])) = state
-mainI state change@(Change (ChangeA1 Type.Polytopes Type.Towrite idx siz) (ChangeA5B7 (s:t))) =
- mainI (mainIH idx state) (mainHF change)
-mainI state change = state
-mainIF :: Int -> State -> State
-mainIF idx (State a b c d e f g) = -- TODO remove aliased regions
- (State (Map.delete idx a) (Map.delete idx b) (Map.delete idx c) (Map.delete idx d) (mainIJ (Map.lookup idx d) e) (mainIJ (Map.lookup idx d) f) g)
-mainIG :: Int -> State -> State
-mainIG idx (State a b c d e f g) = -- TODO filter vertex from each of its tangents
+mainI :: Int -> State -> State
+mainI idx (State a b c d e f g) = -- TODO remove aliased regions
+ (State (Map.delete idx a) (Map.delete idx b) (Map.delete idx c) (Map.delete idx d) (mainL (Map.lookup idx d) e) (mainL (Map.lookup idx d) f) g)
+mainJ :: Int -> State -> State
+mainJ idx (State a b c d e f g) = -- TODO filter vertex from each of its tangents
  (State a b c d (Map.delete idx e) (Map.delete idx f) g)
-mainIH :: Int -> State -> State
-mainIH idx (State a b c d e f g) = -- TODO filter vertex from each of its tangents
+mainK :: Int -> State -> State
+mainK idx (State a b c d e f g) =
  (State a b c d e f (Map.delete idx g))
 
-mainIJ :: (Maybe Nested) -> (IntMap a) -> (IntMap a)
-mainIJ Nothing a = a
-mainIJ (Just (Nested (NestedA1 0 []))) a = a
-mainIJ (Just (Nested (NestedA1 siz (s:t)))) a = mainIJ (Just (Nested (NestedA1 (siz - 1) t))) (Map.delete s a)
+mainL :: (Maybe Nested) -> (IntMap a) -> (IntMap a)
+mainL Nothing a = a
+mainL (Just (Nested (NestedA1 0 []))) a = a
+mainL (Just (Nested (NestedA1 siz (s:t)))) a = mainL (Just (Nested (NestedA1 (siz - 1) t))) (Map.delete s a)
+
+
+
