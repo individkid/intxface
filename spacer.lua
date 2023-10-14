472998d978eb5c38a83372fcea8e43af5be03afd
Paul Coelho <paulrrco@gmail.com>
2023-10-13 22:32:15 -0700

    state change pipe

--- spacer.lua
+++ spacer.lua
@@ -1,6 +1,6 @@
 dofile("type.lua")
-initialize = hideChange("Change(cfg:Emerg(Numerics)vld:Valid(Invalid)idx:Int(0)siz:Int(0))")
-finalize = hideChange("Change(cfg:Emerg(Emergs)vld:Valid(Invalid)idx:Int(0)siz:Int(0))")
+initialize = hideChange("Change(cfg:Emerg(Numerics)vld:Valid(Towrite)idx:Int(0)siz:Int(0))")
+finalize = hideChange("Change(cfg:Emerg(Emergs)vld:Valid(Towrite)idx:Int(0)siz:Int(0))")
 ident = wrapType("Spacez","spacerLua","spaceHs")
 writeChange(initialize,ident)
 writeChange(finalize,ident)
