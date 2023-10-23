bf9f8bd63518f788bba1ba5bea23b35e52b3021e
Paul Coelho <paulrrco@gmail.com>
2023-10-23 11:13:31 -0700

    state change pipe

--- space.hs
+++ space.hs
@@ -17,8 +17,8 @@ import Data.Map
 
 -- Towrite Planes replaces Planes, classifies Halfs, takes Subsets from old with new and old Halfs, appends to Coins/Backs as needed
 -- Towrite Halfs replaces Halfs, samples Planes, takes Subsets from old with new and old Halfs, appends to Coins/Backs as needed
--- Towrite Coins replaces Coins filtered by Halfs, appends to Coins/Backs as needed
--- Towrite Subsets replaces Subsets filtered by Halfs, appends to Coins/Backs as needed
+-- Towrite Coins replaces Coins, appends to Coins/Backs as needed
+-- Towrite Subsets replaces Subsets, appends to Coins/Backs as needed
 -- Toread Planes reads Planes
 -- Toread Halfs reads Halfs
 -- Toread Coins reads Coins
@@ -26,19 +26,13 @@ import Data.Map
 -- Toread Facets calculates Facets from Halfs Subsets Coins
 -- Toread Subsets reads Subsets
 
-newtype Incide = Incide Int deriving (Eq, Ord, Show) -- arbitrary identifier
-newtype Facet = Facet Int deriving (Eq, Ord, Show) -- arbitrary identifier
-
 type Planes = [Plane] -- per boundary
 type Halfs = Space -- per boundary
 type Subsets = [Region] -- in context of space
 type Coins = [[Boundary]] -- per vertex
-type Backs = Map [Boundary] Incide
+type Backs = Map [Boundary] Int
 
 data State = State
- Boundary
- Incide
- Facet
  Planes
  Halfs
  Subsets
@@ -51,7 +45,7 @@ main = getArgs >>= mainF
 mainF :: [String] -> IO ()
 mainF [a] = do
  idx <- wrapIdent Type.Spacez a
- mainG (fromJust idx) (State (Boundary 0) (Incide 0) (Facet 0) [] [] [] [] empty)
+ mainG (fromJust idx) (State [] [] [] [] empty)
  return ()
 
 mainG :: Int -> State -> IO ()
@@ -66,43 +60,53 @@ mainG ifd state = do
 mainH :: Int -> State -> Change -> IO ()
 mainH _ _ (Change (ChangeA1 Type.Emergs _ _ _) _) = return ()
 mainH ifd state (Change (ChangeA1 _ _ _ 0) _) = mainG ifd state
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Towrite idx siz) (ChangeA5B5 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B5 lst)
- set = state -- Towrite Planes replaces Planes, classifies Halfs, takes Subsets from old with new and old Halfs, appends to Coins/Backs as needed
- in mainH ifd set nxt
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Towrite idx siz) (ChangeA5B6 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B6 lst)
- set = state -- Towrite Halfs replaces Halfs, samples Planes, takes Subsets from old with new and old Halfs, appends to Coins/Backs as needed
- in mainH ifd set nxt
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Towrite idx siz) (ChangeA5B7 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B7 lst)
- set = state -- Towrite Coins replaces Coins filtered by Halfs, appends to Coins/Backs as needed
- in mainH ifd set nxt
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Towrite idx siz) (ChangeA5B10 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B10 lst)
- set = state -- Towrite Subsets replaces Subsets filtered by Halfs, appends to Coins/Backs as needed
- in mainH ifd set nxt
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Toread idx siz) (ChangeA5B5 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B5 lst)
- set = state -- Toread Planes reads Planes
- in mainH ifd set nxt
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Toread idx siz) (ChangeA5B6 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B6 lst)
- set = state -- Toread Halfs reads Halfs
- in mainH ifd set nxt
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Toread idx siz) (ChangeA5B7 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B7 lst)
- set = state -- Toread Coins reads Coins
- in mainH ifd set nxt
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Toread idx siz) (ChangeA5B8 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B8 lst)
- set = state -- Toread Points calculates Points from Planes Coins
- in mainH ifd set nxt
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Toread idx siz) (ChangeA5B9 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B9 lst)
- set = state -- Toread Facets calculates Facets from Halfs Subsets Coins
- in mainH ifd set nxt
-mainH ifd state (Change (ChangeA1 tag@Type.Planes act@Type.Toread idx siz) (ChangeA5B10 (fst:lst))) = let
- nxt = Change (ChangeA1 tag act (idx + 1) (siz - 1)) (ChangeA5B10 lst)
- set = state -- Toread Subsets reads Subsets
- in mainH ifd set nxt
+mainH ifd (State a b c d e) (Change (ChangeA1 Type.Planes Type.Towrite idx siz) (ChangeA5B5 val)) = let
+ numeric = mainHE idx a val
+ symbolic = spaceFromPlanes 3 numeric
+ in mainG ifd (mainHK symbolic numeric b c d e)
+mainH ifd (State a b c d e) (Change (ChangeA1 Type.Halfs Type.Towrite idx siz) (ChangeA5B6 val)) = let
+ symbolic = mainHF idx b val
+ numeric = planesFromSpace 3 symbolic
+ in mainG ifd (mainHK symbolic numeric b c d e)
+mainH ifd (State a b c d e) (Change (ChangeA1 Type.Subsets Type.Towrite idx siz) (ChangeA5B10 val)) = let
+ subsets = mainHG idx c val
+ coins = mainHI b subsets d
+ backs = mainHJ coins e
+ in mainG ifd (mainHK b a b subsets coins backs)
+mainH ifd (State a b c d e) (Change (ChangeA1 Type.Coins Type.Towrite idx siz) (ChangeA5B7 val)) = let
+ coins = mainHH idx d val
+ backs = mainHJ coins e
+ in mainG ifd (mainHK b a b c coins backs)
+mainH ifd state@(State a b c d e) (Change (ChangeA1 Type.Planes Type.Toread idx siz) (ChangeA5B5 val)) = do
+ mainG ifd state
+mainH ifd state@(State a b c d e) (Change (ChangeA1 Type.Halfs Type.Toread idx siz) (ChangeA5B6 val)) = do
+ mainG ifd state
+mainH ifd state@(State a b c d e) (Change (ChangeA1 Type.Subsets Type.Toread idx siz) (ChangeA5B10 val)) = do
+ mainG ifd state
+mainH ifd state@(State a b c d e) (Change (ChangeA1 Type.Coins Type.Toread idx siz) (ChangeA5B7 val)) = do
+ mainG ifd state
+mainH ifd state@(State a b c d e) (Change (ChangeA1 Type.Points Type.Toread idx siz) (ChangeA5B8 val)) = do
+ mainG ifd state
+mainH ifd state@(State a b c d e) (Change (ChangeA1 Type.Facets Type.Toread idx siz) (ChangeA5B9 val)) = do
+ mainG ifd state
+mainHE :: Int -> Planes -> [Scalar] -> Planes
+mainHE = undefined -- replace indicated planes, filling in with default as needed
+mainHF :: Int -> Halfs -> [Nested] -> Halfs
+mainHF = undefined -- replace indicated boundaries, filling in with superspace as needed
+mainHG :: Int -> Subsets -> [Int] -> Subsets
+mainHG = undefined -- replace indicated regions, filling in with duplicates as needed
+mainHH :: Int -> Coins -> [Listed] -> Coins
+mainHH = undefined -- replace indicated boundary triplets, filling in with empties as needed
+mainHI :: Halfs -> Subsets -> Coins -> Coins
+mainHI = undefined -- append any boundary triplets of vertices on surface of subsets in halfs
+mainHJ :: Coins -> Backs -> Backs
+mainHJ = undefined -- insert any missing backreferences
+mainHK :: Halfs -> Planes -> Halfs -> Subsets -> Coins -> Backs -> State
+mainHK symbolic a b c d e = let
+ place = spaceToPlace symbolic
+ embed = embedSpace c (spaceToPlace b)
+ regions = takeRegions embed place
+ coins = mainHI symbolic regions d
+ backs = mainHJ coins e
+ in State a b regions coins backs
+
