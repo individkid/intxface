78b797091594ce86b0a5748183801773ea7d31c8
Paul Coelho <paulrrco@gmail.com>
2023-11-04 09:58:57 -0700

    state change pipe

--- space.hs
+++ space.hs
@@ -6,15 +6,15 @@ import Type
 import System.Environment
 import Data.IORef
 import Data.Maybe
-import Data.Map
-import Numeric.LinearAlgebra.Data
+import qualified Data.Map as Map
+import qualified Numeric.LinearAlgebra.Data as Matrix
 
 data State = State
  [Plane]
  Space
  [Region]
  [[Boundary]]
- (Map [Boundary] Int)
+ (Map.Map [Boundary] Int)
 
 main :: IO ()
 main = getArgs >>= mainF
@@ -22,7 +22,7 @@ main = getArgs >>= mainF
 mainF :: [String] -> IO ()
 mainF [a] = do
  idx <- wrapIdent Type.Spacez a
- mainG (fromJust idx) (State [] [] [] [] empty)
+ mainG (fromJust idx) (State [] [] [] [] Map.empty)
  return ()
 
 mainG :: Int -> State -> IO ()
@@ -83,7 +83,7 @@ mainH fdx state@(State a b c d e) (Change (ChangeA1 Type.Points Type.Toread idx 
 mainH fdx state@(State a b c d e) (Change (ChangeA1 Type.Facets Type.Toread 0 0) ChangeA5Bs) = do
  writeChange (Change (ChangeA1 Type.Facets Type.Toresp 0 0) (ChangeA5B10 val)) fdx
  mainG fdx state where
- val = Prelude.map facetToListed (mainN b c e)
+ val = map facetToListed (mainN b c e)
 mainH fdx (State a b c d e) (Change (ChangeA1 Type.Emergs Type.Toadd idx 0) ChangeA5Bs) = let
  corners = mainN b c e
  (boundary,region) = mainL b c d (corners !! idx)
@@ -103,23 +103,23 @@ mainH _ _ (Change (ChangeA1 Type.Emergs _ _ _) _) = return ()
 mainI :: Space -> [Region] -> [[Boundary]] -> [[Boundary]]
 -- append any boundary triplets of vertices on surface of subsets in halfs
 mainI space regions coins = let
- tofold = Prelude.map (mainIF space regions) regions
- in Prelude.foldr (Prelude.++) coins tofold
+ tofold = map (mainIF space regions) regions
+ in foldr (Prelude.++) coins tofold
 mainIF :: Space -> [Region] -> Region -> [[Boundary]]
 mainIF space regions region = let
  attached = attachedBoundaries region space
  triplets = subsets 3 attached
- in Prelude.filter (mainIG space regions) triplets
+ in filter (mainIG space regions) triplets
 mainIG :: Space -> [Region] -> [Boundary] -> Bool
 mainIG space regions boundaries = let
  attached = attachedRegions boundaries space
- shared = Prelude.filter (\x -> Prelude.elem x regions) attached
- count = Prelude.length shared
- in (count /= 0) && (count /= (Prelude.length regions))
+ shared = filter (\x -> elem x regions) attached
+ count = length shared
+ in (count /= 0) && (count /= (length regions))
 
-mainJ :: [[Boundary]] -> (Map [Boundary] Int) -> (Map [Boundary] Int)
+mainJ :: [[Boundary]] -> (Map.Map [Boundary] Int) -> (Map.Map [Boundary] Int)
 -- insert any missing backreferences
-mainJ coins backs = Prelude.foldr (\(x,y) z -> insert y x z) backs (Prelude.zip [0..] coins)
+mainJ coins backs = foldr (\(x,y) z -> Map.insert y x z) backs (zip [0..] coins)
 
 mainK :: Space -> Space -> [Region] -> [Region]
 mainK symbolic b c = let
@@ -129,74 +129,74 @@ mainK symbolic b c = let
 
 mainL :: Space -> [Region] -> [[Boundary]] -> [Int] -> (Boundary,Region)
 mainL space regions coins corners = let
- triplets = Prelude.map (\x -> coins !! x) corners
- boundaries = Prelude.foldr (Naive.++) [] triplets
- shared = Prelude.foldr (Naive.+\) boundaries triplets
+ triplets = map (\x -> coins !! x) corners
+ boundaries = foldr (Naive.++) [] triplets
+ shared = foldr (Naive.+\) boundaries triplets
  attached = attachedRegions shared space
- in (head shared, head (Prelude.filter (mainLF (length boundaries) boundaries space) attached))
+ in (head shared, head (filter (mainLF (length boundaries) boundaries space) attached))
 mainLF :: Int -> [Boundary] -> Space -> Region -> Bool
 mainLF total boundaries space region = (length ((attachedBoundaries region space) Naive.+\ boundaries)) == total
 mainLG :: Space -> Boundary -> Region -> [Region] -> [Region]
 mainLG space boundary region regions = ((oppositeOfRegion [boundary] region space):regions)
 mainLH :: Region -> [Region] -> [Region]
-mainLH region regions = Prelude.filter (\x -> x /= region) regions
+mainLH region regions = filter (\x -> x /= region) regions
 
 mainM :: Int -> Int -> [Plane] -> [[Boundary]] -> [Scalar]
 -- find subcoins, find boundary triples, intersect to points, map to scalars
 mainM idx siz planes coins = let
- nopoint = vectorToPoint (vector [0.0,0.0,0.0])
- subcoins = Prelude.take siz (Prelude.drop idx coins)
- triples = Prelude.map (\x -> Prelude.map (\(Boundary y) -> planes !! y) x) subcoins
- points = Prelude.map (\x -> fromMaybe nopoint (intersectPlanes 3 x)) triples
- in Prelude.map pointToScalar points
+ nopoint = vectorToPoint (Matrix.vector [0.0,0.0,0.0])
+ subcoins = take siz (drop idx coins)
+ triples = map (\x -> map (\(Boundary y) -> planes !! y) x) subcoins
+ points = map (\x -> fromMaybe nopoint (intersectPlanes 3 x)) triples
+ in map pointToScalar points
 
-mainN :: Space -> [Region] -> (Map [Boundary] Int) -> [[Int]]
+mainN :: Space -> [Region] -> (Map.Map [Boundary] Int) -> [[Int]]
 -- find triples of boundary triples, mapped to triples of coin indices
 mainN space regions backs = let
  boundaries = boundariesOfSpace space
- pairs = concat (Prelude.map (\x -> Prelude.map (\y -> (x,y)) (attachedBoundaries x space)) regions)
- polygons = Prelude.filter (\(x,y) -> not (Prelude.elem (oppositeOfRegion [y] x space) regions)) pairs
- triangles = concat (Prelude.map (mainNG space) (Prelude.map (mainNF space regions) polygons))
- in map2 (\x -> fromMaybe 0 (Data.Map.lookup x backs)) triangles
+ pairs = concat (map (\x -> map (\y -> (x,y)) (attachedBoundaries x space)) regions)
+ polygons = filter (\(x,y) -> not (elem (oppositeOfRegion [y] x space) regions)) pairs
+ triangles = concat (map (mainNG space) (map (mainNF space regions) polygons))
+ in map2 (\x -> fromMaybe 0 (Map.lookup x backs)) triangles
 mainNF :: Space -> [Region] -> (Region,Boundary) -> [[Boundary]]
 -- find corners of polygon
 mainNF space regions (region,boundary) = let
  attached = attachedBoundaries region space
- triples = Prelude.filter (\x -> Prelude.elem boundary x) (subsets 2 attached)
- corners = Prelude.filter (\x -> oppositeOfRegionExists x region space) triples
- in Prelude.map (Prelude.filter (\x -> x /= boundary)) corners
+ triples = filter (\x -> elem boundary x) (subsets 2 attached)
+ corners = filter (\x -> oppositeOfRegionExists x region space) triples
+ in map (filter (\x -> x /= boundary)) corners
 mainNG :: Space -> [[Boundary]] -> [[[Boundary]]]
 -- find triangles of corners
 mainNG _ [first,second,third] = [[first,second,third]]
 mainNG space corners = let
  (first:second:rest) = corners
- ordered = Prelude.zip [0..] (mainNH corners)
- one = Prelude.foldr (\(x,y) z -> if (y == first) then x else z) 0 ordered
- other = Prelude.foldr (\(x,y) z -> if (y == second) then x else z) 0 ordered
- before = Prelude.map snd (Prelude.filter (\(x,_) -> x == other || x <= one) ordered)
- after = Prelude.map snd (Prelude.filter (\(x,_) -> x == other || x >= one) ordered)
+ ordered = zip [0..] (mainNH corners)
+ one = foldr (\(x,y) z -> if (y == first) then x else z) 0 ordered
+ other = foldr (\(x,y) z -> if (y == second) then x else z) 0 ordered
+ before = map snd (filter (\(x,_) -> x == other || x <= one) ordered)
+ after = map snd (filter (\(x,_) -> x == other || x >= one) ordered)
  in (mainNG space before) Prelude.++ (mainNG space after)
 mainNH :: [[Boundary]] -> [[Boundary]]
 -- successor shares boundaries with predecessor
 mainNH vertices = let
- boundaries = Prelude.foldr (Naive.++) [] vertices
- (boundary:_) = Prelude.foldr (Naive.+\) boundaries vertices
- ((one:others):rest) = Prelude.map (\x -> x Naive.\\ [boundary]) vertices
+ boundaries = foldr (Naive.++) [] vertices
+ (boundary:_) = foldr (Naive.+\) boundaries vertices
+ ((one:others):rest) = map (\x -> x Naive.\\ [boundary]) vertices
  corners = mainNK (mainNI ((one:others):rest)) (one,(one:others)) (one,(one:others)) []
- in Prelude.map (\x -> boundary:x) corners
-mainNI :: [[Boundary]] -> (Map Boundary [[Boundary]])
+ in map (\x -> boundary:x) corners
+mainNI :: [[Boundary]] -> (Map.Map Boundary [[Boundary]])
 -- map boundaries of corners to corner pairs
-mainNI corners = Prelude.foldr (\x a -> Prelude.foldr (\y b -> mainNJ y x (Data.Map.lookup y b) b) a x) empty corners
-mainNJ :: Boundary -> [Boundary] -> (Maybe [[Boundary]]) -> (Map Boundary [[Boundary]]) -> (Map Boundary [[Boundary]])
-mainNJ boundary corner (Just sofar) backref = Data.Map.insert boundary (corner:sofar) backref
-mainNJ boundary corner Nothing backref = Data.Map.insert boundary [corner] backref
-mainNK :: (Map Boundary [[Boundary]]) -> (Boundary,[Boundary]) -> (Boundary,[Boundary]) -> [[Boundary]] -> [[Boundary]]
+mainNI corners = foldr (\x a -> foldr (\y b -> mainNJ y x (Map.lookup y b) b) a x) Map.empty corners
+mainNJ :: Boundary -> [Boundary] -> (Maybe [[Boundary]]) -> (Map.Map Boundary [[Boundary]]) -> (Map.Map Boundary [[Boundary]])
+mainNJ boundary corner (Just sofar) backref = Map.insert boundary (corner:sofar) backref
+mainNJ boundary corner Nothing backref = Map.insert boundary [corner] backref
+mainNK :: (Map.Map Boundary [[Boundary]]) -> (Boundary,[Boundary]) -> (Boundary,[Boundary]) -> [[Boundary]] -> [[Boundary]]
 -- recurse to find next tuple to append to list
 mainNK backref first next@(boundary,corner) done
  | next == first = done
  | otherwise = mainNK backref first (other,found) (found:done) where
- found = Prelude.head (Prelude.filter (\x -> x /= corner) (fromMaybe [] (Data.Map.lookup boundary backref)))
- other = Prelude.head (Prelude.filter (\x -> x /= boundary) found)
+ found = head (filter (\x -> x /= corner) (fromMaybe [] (Map.lookup boundary backref)))
+ other = head (filter (\x -> x /= boundary) found)
 
 scalarToPlane :: Int -> [Plane] -> [Scalar] -> [Plane]
 scalarToPlane = undefined -- replace indicated planes, filling in with default as needed
