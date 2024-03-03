9a6abca11b8404407beb47a4f1823172df213657
Paul Coelho <paulrrco@gmail.com>
2024-03-03 07:18:21 -0800

    depend argument generate

--- depend.lua
+++ depend.lua
@@ -95,7 +95,7 @@ function makecopy(values,target,suf)
 	local file = io.open("subdir."..match.."/"..match,"r")
 	local depender = getdepender(target) -- faceC.o
 	indent = indent + 1; indentwrite("makecopy: subdir."..match.."/"..match.." to subdir."..target.."\n",indent)
-	if file == nil and not trymake({{},{}},match) == nil then indent = indent - 1; return false end
+	if file == nil and not trymake({{},{}},match) then indent = indent - 1; return false end
 	if file ~= nil then io.close(file) end
 	copydepender(match,depends)
 	adddepender(match,depends,depender)
@@ -111,7 +111,7 @@ function remakecopy(values,target,suf)
 	indent = indent + 1; indentwrite("remakecopy: "..match.." "..target.." "..depender.."\n",indent)
 	copydepender(depender,deps)
 	adddepender(match,deps,depender)
-	if trymake({deps,{}},depender) == nil then indent = indent - 1; return false end
+	if not trymake({deps,{}},depender) then indent = indent - 1; return false end
 	copydepender(depender,depends)
 	os.execute("cp subdir."..depender.."/"..depender.." subdir."..target.."/")
 	indent = indent - 1; return true
@@ -120,6 +120,7 @@ end
 function copymake(values,target,ext)
 	local match = values[3]
 	local extras = values[2]
+	if (extras[match] == nil) then extras[match] = {}; extras[match][target] = {} end
 	indent = indent + 1; indentwrite("copymake: "..target.."\n",indent)
 	writeout("subdir."..target.."/"..match..".mk",extras[match])
 	indent = indent - 1; return true
@@ -170,8 +171,8 @@ end
 function trymatch(values,target)
 	indent = indent + 1; indentwrite("trymatch: "..target.."\n",indent)
 	for line in io.lines("stderr."..target) do if uname == "Linux" then
-		prefix = "1a"; if callmatch(values,line,"^/bin/sh: [%d]*: ./([%w]*C): not found$") and makecopy(values,target,"") then indent = indent - 1; return true end
-		prefix = "1b"; if callmatch(values,line,"^/bin/sh: [%d]*: ./([%w]*Lua): not found$") and makecopy(values,target,"") then indent = indent - 1; return true end
+		prefix = "1a "; if callmatch(values,line,"^/bin/sh: [%d]*: ./([%w]*C): not found$") and makecopy(values,target,"") then indent = indent - 1; return true end
+		prefix = "1b "; if callmatch(values,line,"^/bin/sh: [%d]*: ./([%w]*Lua): not found$") and makecopy(values,target,"") then indent = indent - 1; return true end
 		prefix = "1c "; if callmatch(values,line,"^/bin/sh: [%d]*: ./([%w]*Hs): not found$") and makecopy(values,target,"") then indent = indent - 1; return true end
 		-- prefix = "2a "; if callmatch(values,line,"^make: *** No rule to make target '([%w]*)C'.  Stop.$") and copysource(values,target,"c") then indent = indent - 1; return true end
 		prefix = "2b "; if callmatch(values,line,"^make: *** No rule to make target '([%w]*)C.o'.  Stop.$") and makecopy(values,target,".c") then indent = indent - 1; return true end
@@ -216,7 +217,8 @@ function trymatch(values,target)
 		prefix = "8b "; if callmatch(values,line,"^lua: cannot open ([%w]*).lua: No such file or directory$") and makecopy(values,target,".lua") then indent = indent - 1; return true end
 		prefix = "9a "; if callmatch(values,line,"^lua: [%w./]*:[%d]*: module '([%w]*)' not found:$") and makecopy(values,target,".so") then indent = indent - 1; return true end
 		prefix = "10a "; if callmatch(values,line,"[%w./]*: undefined symbol: ([%w]*)$") and findsource(values,"^[^[:space:]][^[:space:]]* *","\\(.*\\)$","cpp","Cpp.o") and remakecopy(values,target,"") then indent = indent - 1; return true end
-		prefix = "10a "; if callmatch(values,line,"[%w./]*: undefined symbol: ([%w]*)$") and findsource(values,"^[^[:space:]][^[:space:]]* *","\\(.*\\)$","c","C.o") and remakecopy(values,target,"") then indent = indent - 1; return true end
+		prefix = "10b "; if callmatch(values,line,"[%w./]*: undefined symbol: ([%w]*)$") and findsource(values,"^[^[:space:]][^[:space:]]* *","\\(.*\\)$","c","C.o") and remakecopy(values,target,"") then indent = indent - 1; return true end
+		prefix = "11a "; if callmatch(values,line,"^error:([%w]*).c: make passed but build failed$") and copymake(values,target,".c") then indent = indent - 1; return true end
 	elseif uname == "Darwin" then
 		prefix = "1a "; if callmatch(values,line,"^/bin/sh: ./([%w]*C): No such file or directory$") and makecopy(values,target,"") then indent = indent - 1; return true end
 		prefix = "1b "; if callmatch(values,line,"^/bin/sh: ./([%w]*Lua): No such file or directory$") and makecopy(values,target,"") then indent = indent - 1; return true end
@@ -296,6 +298,9 @@ function trymake(values,target)
 		writeout("subdir."..target.."/depend.mk",depends)
 		indentwrite("trymake: make: "..target.."\n",indent)
 		os.execute("make -d -C subdir."..target.." "..target.." 2> stderr."..target.." >> stdout."..target)
+		local file = io.open("subdir."..target.."/"..target,"r");
+		if (file == nil) then os.execute("echo error:"..target..": make passed but build failed >> stderr."..target)
+		else io.close(file) end
 		local done = true
 		for line in io.lines("stderr."..target) do done = false end
 		if done then
@@ -322,7 +327,7 @@ for line in io.lines("Makefile") do
 end
 for key,val in ipairs(targets) do
 	local values = {{},{}}
-	if not trymake(values,val) then io.stderr("trymake failed"); os.exit(-1) end
+	if not trymake(values,val) then io.stderr("trymake failed\n"); os.exit(-1) end
 	for k,v in pairs(values[1]) do
 		if (depends[k] == nil) then depends[k] = {} end
 		for ky,vl in pairs(v) do depends[k][ky] = true end
