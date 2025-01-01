6d21cf8b714fd17219f78c895a89a745f85336a6
Paul Coelho <paulrrco@gmail.com>
2024-12-31 16:10:35 -0800

    base item thread

--- vulkan.cpp
+++ vulkan.cpp
@@ -35,30 +35,6 @@ struct TestVertex {
     glm::vec3 pos;
     glm::vec3 color;
     glm::vec2 texCoord;
-    // TODO replace these by wrappers around Constant functions from type.h
-    static VkVertexInputBindingDescription getBindingDescription() {
-        VkVertexInputBindingDescription bindingDescription{};
-        bindingDescription.binding = 0;
-        bindingDescription.stride = sizeof(TestVertex);
-        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-        return bindingDescription;
-    }
-    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions() {
-        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
-        attributeDescriptions[0].binding = 0;
-        attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[0].offset = offsetof(TestVertex, pos);
-        attributeDescriptions[1].binding = 0;
-        attributeDescriptions[1].location = 1;
-        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[1].offset = offsetof(TestVertex, color);
-        attributeDescriptions[2].binding = 0;
-        attributeDescriptions[2].location = 2;
-        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
-        attributeDescriptions[2].offset = offsetof(TestVertex, texCoord);
-        return attributeDescriptions;
-    }
 };
 struct UniformBufferObject {
     alignas(16) glm::mat4 model;
