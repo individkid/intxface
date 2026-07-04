b2857e35bd5e42e4c88ecb77dbf2e4ba99f3a199
Paul Coelho <paulrrco@gmail.com>
2026-07-03 21:32:47 -0700

    permutate manipulate decorate

--- planra.sh
+++ planra.sh
@@ -14,6 +14,4 @@ echo "Random choice: $random_string"
 	'$(CenterSiz := #1)' '$(CenterMem := @kernel)' 'Tsage(siz:2sav[0]:CenterMemsav[1]:CenterSizidx[0]:$(@kernel))'\
 	'Eval(res[0]:$(@kernel)fnc[0]:$(Non Put Imm @_ Op EndOp Op ; @_ Op))'\
 	'Transferz(siz:1idx:0slf:-1exe[0]:Machine(xfr:Voidexp[0]:$(Non ManipLeft := #-20 ManipBase := #-20 ManipFixed := #133 Op)))'\
-	'$(CenterSiz := #1)' 'Tsage(siz:1sav[0]:CenterSizidx[0]:$(@string))'\
-	'Eval(res[0]:$(@memorys)fnc[0]:$(temp = @_))'\
-	'Eval(res[0]:$(@string)fnc[0]:$(Fld @_ str Imm @temp Op #0 Op))'
+	'Dopy(sig:2arg[0]:$(@memorys)arg[1]:$(@transfer))'
